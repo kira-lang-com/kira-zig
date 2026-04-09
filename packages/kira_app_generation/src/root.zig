256cd2aebd21bdfec5cb1f3c93f302d04a4089a0
Priam Chambouvet <aipriamc@gmail.com>
2026-04-09 16:57:42 +0200

    Fix library template scaffolding

--- packages/kira_app_generation/src/root.zig
+++ packages/kira_app_generation/src/root.zig
@@ -1,1 +1,3 @@
 pub const generateApp = @import("generator.zig").generateApp;
+pub const generate = @import("generator.zig").generate;
+pub const TemplateKind = @import("generator.zig").TemplateKind;
--- packages/kira_app_generation/src/templates.zig
+++ packages/kira_app_generation/src/templates.zig
@@ -1,35 +1,56 @@
 const std = @import("std");
 
 pub fn copyTemplateTree(allocator: std.mem.Allocator, src_path: []const u8, dst_path: []const u8, app_name: []const u8) !void {
-    _ = allocator;
     try std.fs.cwd().makePath(dst_path);
-    try copyDirRecursive(src_path, dst_path, app_name);
+    try copyDirRecursive(allocator, src_path, dst_path, app_name);
 }
 
-fn copyDirRecursive(src_path: []const u8, dst_path: []const u8, app_name: []const u8) !void {
+fn copyDirRecursive(allocator: std.mem.Allocator, src_path: []const u8, dst_path: []const u8, app_name: []const u8) !void {
     var src_dir = try std.fs.cwd().openDir(src_path, .{ .iterate = true });
     defer src_dir.close();
 
     var iterator = src_dir.iterate();
     while (try iterator.next()) |entry| {
         const child_src = try std.fs.path.join(std.heap.page_allocator, &.{ src_path, entry.name });
-        const child_dst = try std.fs.path.join(std.heap.page_allocator, &.{ dst_path, entry.name });
+        const rendered_name = try renderTemplateName(allocator, entry.name, app_name);
+        defer allocator.free(rendered_name);
+        const child_dst = try std.fs.path.join(std.heap.page_allocator, &.{ dst_path, rendered_name });
         defer std.heap.page_allocator.free(child_src);
         defer std.heap.page_allocator.free(child_dst);
 
         switch (entry.kind) {
             .directory => {
                 try std.fs.cwd().makePath(child_dst);
-                try copyDirRecursive(child_src, child_dst, app_name);
+                try copyDirRecursive(allocator, child_src, child_dst, app_name);
             },
             .file => {
                 const contents = try std.fs.cwd().readFileAlloc(std.heap.page_allocator, child_src, 1024 * 1024);
                 defer std.heap.page_allocator.free(contents);
-                const rendered = try std.mem.replaceOwned(u8, std.heap.page_allocator, contents, "DemoApp", app_name);
-                defer std.heap.page_allocator.free(rendered);
+                const rendered = try renderTemplateContents(allocator, contents, app_name);
+                defer allocator.free(rendered);
                 try std.fs.cwd().writeFile(.{ .sub_path = child_dst, .data = rendered });
             },
             else => {},
         }
     }
+}
+
+fn renderTemplateContents(allocator: std.mem.Allocator, contents: []const u8, app_name: []const u8) ![]u8 {
+    const lower_name = try std.ascii.allocLowerString(allocator, app_name);
+    defer allocator.free(lower_name);
+
+    const with_app = try std.mem.replaceOwned(u8, allocator, contents, "DemoApp", app_name);
+    defer allocator.free(with_app);
+    const with_library = try std.mem.replaceOwned(u8, allocator, with_app, "DemoLibrary", app_name);
+    defer allocator.free(with_library);
+    return std.mem.replaceOwned(u8, allocator, with_library, "demolibrary", lower_name);
+}
+
+fn renderTemplateName(allocator: std.mem.Allocator, name: []const u8, app_name: []const u8) ![]u8 {
+    const lower_name = try std.ascii.allocLowerString(allocator, app_name);
+    defer allocator.free(lower_name);
+
+    const with_library = try std.mem.replaceOwned(u8, allocator, name, "DemoLibrary", app_name);
+    defer allocator.free(with_library);
+    return std.mem.replaceOwned(u8, allocator, with_library, "demolibrary", lower_name);
 }
